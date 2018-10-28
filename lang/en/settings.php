bd1eaddae013928eda9c4cd366a3e1d5d2fbe5a8
peterfromearth <coder@peterfromearth.de>
2018-10-28 10:11:11 +0100

    update eng language

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,3 +1,6 @@
 <?php
 
 $lang['max_hours'] = 'How many hours per day are allowed?';
+$lang['weekdays'] = 'How many weekdays from monday on';
+$lang['namespace_allowed'] = 'Allowed namespaced, space separated';
+$lang['days_recent_project_active'] = 'Number of day when project should not be in recent project anymore';
